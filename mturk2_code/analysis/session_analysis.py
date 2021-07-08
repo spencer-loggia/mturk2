0db5cde14152b264213ee79749f8b7925ffdb944
Spencer R. Loggia <spencer.loggia98@gmail.com>
2021-07-08 12:50:18 -0400

    update file search, add some analysis, add plotting

--- mturk2_code/analysis/session_analysis.py
+++ mturk2_code/analysis/session_analysis.py
@@ -8,7 +8,8 @@ import os
 import re
 import datetime
 import copy
-from scipy import stats
+from matplotlib import pyplot as plt
+import pandas as pd
 
 SUBJECT_NAMES = {'Buzz', 'Tina', 'Yuri', 'Sally'}
 VERSION_NOTE = "The above is preliminary analysis, more detailed / interesting information will be added."
@@ -24,12 +25,13 @@ class SessionData:
         self.monkey_name = items[6]
         self.mode = items[7]
         self.tablet_used = items[-1]
-        self.data_dict =data_dict
+        self.data_dict = data_dict
         self.shape_trials = np.array(self.data_dict['Test'], dtype=int)
         self.color_trials = np.array(self.data_dict['TestC'], dtype=int)
         self.reward_map = np.array(self.data_dict['RewardStage'], dtype=int)
         self.choices = np.array(self.data_dict['Response'], dtype=int)
-        self.trial_time_microseconds = self.data_dict['StartTime']
+        self.trial_time_milliseconds = self.data_dict['StartTime']
+
 
     def get_priors(self):
         """
@@ -48,24 +50,34 @@ class SessionData:
         freq = np.unique(dist, return_counts=True)[1]
         return freq
 
+    def get_max_reward_prob(self):
+        """
+        get the observed probability that a monkey picked the best reward on a trial
+        """
+        best_choice = np.max(self.reward_map, axis=1)
+        num_best = np.count_nonzero(best_choice == np.choose(self.choices, self.reward_map.T))
+        return num_best / len(self)
+
     def __len__(self):
         return len(self.data_dict['StartTime'])
 
 
 def analyze_session(filename, data_dict):
+    """
+    Initialize data analysis and collect results.
+    Return text descripters at index 0, with further indices being data for plotting
+    Store any historical data to csv.
+    """
     data = SessionData(filename, data_dict)
     observed_r_dist = data.get_real_reward_dist()
     prior_r_dist = data.get_priors()
-    out = ''
-    out += "Subject: " + str(data.monkey_name) + '\n\n'
-    out += "Session Date: " + str(data.date) + '\n\n'
-    out += "Trials Completed: " + str(len(data)) + '\n\n'
-    out += "Session Runtime: " + str((data.trial_time_microseconds[-1] / 1e3) / (60*60)) + " hours" + '\n\n'
-    out += "Frequency Subject Received Each Reward Type (Worst to Best): " + str(list(observed_r_dist)) + '\n\n'
-    out += "Chance Frequency of Each Reward Type (Worst to Best): " + str(list(prior_r_dist * len(data))) + '\n\n'
-    return out
+    analysis = {'observed_reward_dist': observed_r_dist,
+                'prior_reward_dist': prior_r_dist * len(data),
+                'percent_diff_chance': ((observed_r_dist - (prior_r_dist  * len(data))) / (prior_r_dist * len(data))),
+                'percent_best_reward': data.get_max_reward_prob()}
+    return analysis, data
 
-def dropbox_connector():
+def dropbox_connector(access_token: str):
     with dropbox.Dropbox(oauth2_access_token='rs2UxmS43BwAAAAAAAAAAXfNzkhWFgemApwtub7zhgWix78XTKrNQCQnyFOgE7zt') as dbx:
         try:
             print(dbx.users_get_current_account())
@@ -76,20 +88,62 @@ def dropbox_connector():
 
 
 def get_session_data(dbx, date: datetime.date) -> List[Tuple[str, Dict]]:
-    date_str = date.strftime("%Y-%m-%d")
+    name_set = copy.deepcopy(SUBJECT_NAMES)
     subject_data = []
-    for n in SUBJECT_NAMES:
-        args = dropbox.files.SearchOptions(path='/Apps/ShapeColorSpace/MonkData',
-                                           max_results=10)
-        res = dbx.files_search_v2(query=date_str + ' ' + n, options=args)
-        for r in res.matches:
-            if r.metadata._value.size > 100 and n in r.metadata._value.name:
-                metadata, fdata = dbx.files_download(
-                    path='/Apps/ShapeColorSpace/MonkData/' + r.metadata._value.name)
-                subject_data.append((r.metadata._value.name, json.loads(fdata.content)[0]))
+    res = dbx.files_list_folder('/Apps/ShapeColorSpace/MonkData/')
+    complete = False
+    while not complete:
+        for f in res.entries:
+            if len(name_set) == 0:
                 break
+            items = re.split('-|_', f.name)
+            try:
+                sname = items[6]
+            except IndexError:
+                continue
+            if f.size > 0 and sname in name_set:
+                reported_datetime = f.client_modified
+                if reported_datetime.year != date.year or reported_datetime.month != date.month or reported_datetime.day != date.day:
+                    continue
+                name_set.remove(sname)
+                metadata, fdata = dbx.files_download(
+                    path='/Apps/ShapeColorSpace/MonkData/' + f.name)
+                subject_data.append((f.name, json.loads(fdata.content)[0]))
+        if res.has_more:
+            res = dbx.files_list_folder_continue(res.cursor)
+        else:
+            complete = True
+
     print('Loaded File Data Successfully')
     return subject_data
+
+def handler(subject_data: list):
+    out = ''
+    colors = ['red', 'green', 'blue', 'purple']
+    for i, s in enumerate(subject_data):
+        analysis, data = analyze_session(s[0], s[1])
+        out += '---------------------------------------\n'
+        out += "Subject: " + str(data.monkey_name) + '\n\n'
+        out += "Session Date: " + str(data.date) + '\n\n'
+        out += "Trials Completed: " + str(len(data)) + '\n\n'
+        out += "Session Runtime: " + str((data.trial_time_milliseconds[-1] / 1e3) / (60 * 60)) + " hours" + '\n\n'
+        out += "Frequency Subject Received Each Reward Type (Worst to Best): " + str(list(analysis['observed_reward_dist'])) + '\n\n'
+        out += "Chance Frequency of Each Reward Type (Worst to Best): " + str(list(analysis['prior_reward_dist'])) + '\n\n'
+        out += "Percent Difference of Observed vs Chance: " + str(list(analysis['percent_diff_chance'] * 100)) + "\n\n"
+        out += "Observed Portion of Trials Subject Chose Best Available Reward: " + str(analysis['percent_best_reward']) + '\n\n'
+        plt.plot(np.arange(4), analysis['percent_diff_chance'].reshape(-1),
+                 color=colors[i],
+                 label=data.monkey_name,
+                 linestyle='--',
+                 marker='o')
+        plt.xlabel("Reward Type (Worst to Best)")
+        plt.ylabel("Reward Frequency Percent Difference From Chance ")
+        plt.title("MTurk2 Subject Performance " + str(data.date))
+        plt.legend()
+    plt.suptitle('')
+    plt.savefig('../saved_data/figures/' + str(data.date) + '_performance_vs_chance_mturk2.png')
+    return out
+
 
 
 def communicate(psswd, message):
@@ -102,26 +156,30 @@ def communicate(psswd, message):
         server.login("mturk2mailserverSL@gmail.com", psswd)
         server.sendmail(from_addr="mturk2mailserverSL@gmail.com",
                         to_addrs=["spencer.loggia@nih.gov",
-                                  "stuart.duffield@nih.gov",
-                                  "shriya.awasthi@nih.gov",
-                                  "bevil.conway@nih.gov",
-                                  "tunk.tunk@icloud.com"],
+                                  ],
                         msg=message,)
         print("Report Email Delivered Successfully.")
 
 
 if __name__=='__main__':
-    dbx = dropbox_connector()
     try:
         date = datetime.date(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+        psswd = sys.argv[4]
+        dbx_token = sys.argv[5]
+        mode = sys.argv[6]
     except (IndexError, ValueError):
         date = datetime.date.today()
+        psswd = sys.argv[1]
+        dbx_token = sys.argv[2]
+        mode = sys.argv[3]
+    if mode not in ['--prod', '--test']:
+        raise ValueError('Mode must be --prod or --test.')
+    dbx = dropbox_connector(dbx_token)
     subject_data = get_session_data(dbx, date)
     output = 'MTurk 2 Progress Report for ' + str(date) + '\n'
     if len(subject_data) == 0:
         output += 'MTurk2 boxes were not setup today.'
-    for s in subject_data:
-        output += '---------------------------------------\n'
-        output += analyze_session(s[0], s[1])
+    output += handler(subject_data)
     print(output)
-    communicate(sys.argv[1], output)
+    if mode == '--prod':
+        communicate(psswd, output)
--- mturk2_code/analysis_runner.bat
+++ mturk2_code/analysis_runner.bat
@@ -1,2 +1,2 @@
 cd "C:\Users\loggiasr\projects\monkey_turk_2\mturk2_code\analysis"
-C:\Users\loggiasr\Anaconda3\envs\sl_test\python.exe session_analysis.py "HappyBirthdayToMe"
+C:\Users\loggiasr\Anaconda3\envs\sl_test\python.exe session_analysis.py "HappyBirthdayToMe" "rs2UxmS43BwAAAAAAAAAAXfNzkhWFgemApwtub7zhgWix78XTKrNQCQnyFOgE7zt" --prod
