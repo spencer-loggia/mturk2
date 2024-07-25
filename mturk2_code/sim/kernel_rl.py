a4a933aaddb8c36aea4d355fe82fe39b197677e6
Spencer R. Loggia <spencer.loggia98@gmail.com>
2024-07-25 13:58:27 -0400

    add dynamic kernel to RL fit

--- mturk2_code/sim/kernel_rl.py
+++ mturk2_code/sim/kernel_rl.py
@@ -4,13 +4,14 @@ import numpy as np
 from neurotools import util
 
 class KernelRL:
-    def __init__(self, device='cpu', precision="single", resolution=72, temporal_window=20):
+    def __init__(self, device='cpu', precision="single", resolution=72, temporal_window=20, expected_plateu=100000, kernel_mode="logistic"):
         """
         Fits monkey behavior by updating with a parameterized gaussian kernel.
         """
         self.s_real = 36
         self.c_real = 36
         self.resolution = round(resolution)
+        self.dynamic_kernel = kernel_mode == "logistic"
 
         if precision == "single":
             self.ndtype = torch.float32
@@ -25,62 +26,136 @@ class KernelRL:
 
         self.init_prob = torch.nn.Parameter(torch.zeros((resolution,
                                                          resolution), device=device, dtype=self.ndtype))
-        self.kernel_cov = torch.nn.Parameter(torch.eye(2, device=device, dtype=self.ndtype) * .5)
+        self.init_kernel_cov = torch.nn.Parameter(torch.tensor([.5, .01, .5], device=device, dtype=self.ndtype))
+        if self.dynamic_kernel:
+            # compute initial tau to reach 90% final at 1/2 number of trials 
+            tau = -2 * torch.log(torch.tensor([.1 / 1.9], dtype=self.ndtype, device=device)) / expected_plateu
+            self.kernel_log_tau = torch.nn.Parameter(torch.log(tau))
+            self.final_kernel_cov = torch.nn.Parameter(self.init_kernel_cov.clone())
 
         self.log_lr = torch.nn.Parameter(torch.tensor([-2], device=device, dtype=self.ndtype))
 
         self.inverse_temperature = torch.nn.Parameter(torch.ones(1, device=device, dtype=self.ndtype))
+        
+        self.state_spaces = []
+        self.loss_hist = []
 
-        self.optimizer = torch.optim.Adam(params=[self.init_prob, self.kernel_cov, self.log_lr, self.inverse_temperature], lr=.01)
+        self.optimizer = None
+        
+    def to(self, device):
+        self.init_prob = torch.nn.Parameter(self.init_prob.to(device))
+        self.init_kernel_cov = torch.nn.Parameter(self.init_kernel_cov.to(device))
+        if self.dynamic_kernel:
+            self.kernel_log_tau = torch.nn.Parameter(self.kernel_log_tau.to(device))
+            self.final_kernel_cov = torch.nn.Parameter(self.final_kernel_cov.to(device))
+        self.log_lr = torch.nn.Parameter(self.log_lr.to(device))
+        self.inverse_temperature = torch.nn.Parameter(self.inverse_temperature.to(device))
+    
+    def _get_choices_rewards(self, trial_data):
+        """
+        <n, 4, 2> choices on range (0, s_real | c_real)
+        """
+        raw_choices = trial_data[["c1", "s1", "c2", "s2", "c3", "s3", "c4", "s4"]].to_numpy().reshape((-1, 4, 2))
+        raw_choices = torch.from_numpy(raw_choices).to(self.ndtype).to(self.device)
+        choices = raw_choices / torch.tensor([self.c_real, self.s_real], dtype=self.ndtype, device=self.device)[None, None, :]
+        choice_idx = torch.from_numpy(trial_data.choice_idx.to_numpy()).long().to(self.device)
+        reward = torch.from_numpy(trial_data.reward.to_numpy()).to(self.ndtype).to(self.device)
+        return torch.round(self.resolution * choices).int(), choice_idx, reward
 
-    def fit(self, trial_data:pd.DataFrame, epochs=1000):
+    def _nonsingular_cov_from_triu(self, vec):
+        cov_in = util.triu_to_square(vec, 2, includes_diag=True)
+        return cov_in.clone() @ cov_in.clone()
+
+    def fit(self, trial_data:pd.DataFrame, epochs=1000, lr=.01):
         """
         trial_data: DataFrame: cols: s1-s4, c1-c4, reward, choice_idx
         """
         ce = torch.nn.CrossEntropyLoss()
         state_spaces = None
+        options, choice_idxs, rewards = self._get_choices_rewards(trial_data)
+        if not self.dynamic_kernel:
+            self.optimizer = torch.optim.Adam(params=[self.init_prob, self.init_kernel_cov,
+                                                      self.inverse_temperature, self.log_lr],
+                                              lr=lr)
+        else:
+            self.optimizer = torch.optim.Adam(params=[self.init_kernel_cov, self.kernel_log_tau,
+                                                      self.final_kernel_cov, self.inverse_temperature, self.log_lr],
+                                              lr=lr)
         for epoch in range(epochs):
-            state_spaces = []
+            loss = torch.zeros((1,), dtype=self.ndtype, device=self.device)
+            self.state_spaces = []
             print("Epoch", epoch)
             self.optimizer.zero_grad()
             state_space = self.init_prob.clone()
-            # construct gaussian kernel (uses spencer's tools library)
-            for ind, trial in trial_data.iterrows():
-                print("trial", ind)
-                kernel_cov = self.kernel_cov.clone() @ self.kernel_cov.T.clone()
+            # iterate through all trials
+
+            for ind in range(len(trial_data)):
+                # build true nonsingular cov from cov input
+                kernel_cov = self._nonsingular_cov_from_triu(self.init_kernel_cov)
+                if self.dynamic_kernel:
+                    f_kernel_cov = self._nonsingular_cov_from_triu(self.final_kernel_cov)
+                    # select the input covariance matrix at this time point
+                    kernel_cov = util.logistic_func(torch.tensor([ind]), kernel_cov.flatten(),
+                                                 f_kernel_cov.flatten(), self.kernel_log_tau).reshape((2, 2))
+                # construct gaussian kernel (uses spencer's tools library)
                 kernel = util.gaussian_kernel(kernel_size=(self.resolution, self.resolution),
                                               cov=kernel_cov * self.resolution,
                                               integral_resolution=1,
-                                              renormalize=False)
-                raw_choices = torch.tensor([[trial.c1, trial.s1],
-                                       [trial.c2, trial.s2],
-                                       [trial.c3, trial.s3],
-                                       [trial.c4, trial.s4]], dtype=self.ndtype, device=self.device)
+                                              renormalize=True,)
                 # to (0, 1)
-                choices = raw_choices / torch.tensor([self.c_real, self.s_real]).unsqueeze(0)
-                reward = torch.tensor([trial.reward], dtype=self.ndtype, device=self.device)
-                choice_idx = int(trial.choice_idx)
+                reward = rewards[ind]
+                # choice_idx = int(trial.choice_idx)
                 # get the expected reward of each option from most recent state space
-                state_space_indexes = torch.round(choices * self.resolution).int()
-                option_reward_exp = state_space[state_space_indexes[:, 0], state_space_indexes[:, 1]]
+                option_reward_exp = state_space[options[ind, :, 0], options[ind, :, 1]]
                 # compute softmax
                 choice_probs = torch.softmax(option_reward_exp * self.inverse_temperature, dim=0)
                 # compare to monkey choice
-                loss = ce(choice_probs, torch.tensor(choice_idx))
-                # propagate gradient
-                loss.backward(retain_graph=False)
-                self.optimizer.step()
-                self.optimizer.zero_grad()
+                choice_idx = choice_idxs[ind]
+                loss = loss - choice_probs[choice_idx]
                 # add state space to list
-                state_spaces.append(state_space.clone())
+                self.state_spaces.append(state_space.clone())
                 # # update reward space
-                mu = state_space_indexes[choice_idx].detach().tolist()
-                state_space = state_spaces[-1].detach().clone()
+                offset_mu = (options[ind, choice_idx] - self.resolution // 2).detach().cpu().tolist() # how much to translate kernel
+                state_space = self.state_spaces[-1].clone()
                 # # center kernel on choice
-                centered_kernel = torch.roll(kernel.clone(), mu, dims=(0, 1))
-                state_space = state_space + torch.exp(self.log_lr) * (centered_kernel * reward.detach() - state_space)
+                centered_kernel = torch.roll(kernel.clone(), offset_mu, dims=(0, 1))
+                state_space = state_space + torch.exp(self.log_lr) * centered_kernel * (reward.detach() - state_space)  # kernel need to wieght both observed reward and current space
+            # propagate gradient
+            loss.backward(retain_graph=False)
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            self.loss_hist.append(loss.detach().cpu().item())
+            # check early stopping:
+            if epoch > 50:
+                states = self.optimizer.state_dict()["state"]
+                moments = []
+                for p in states.keys():
+                    moments.append(states[p]["exp_avg"].flatten())
+                moment = torch.abs(torch.concat(moments, dim=0)).mean()
+                print(moment)
+                if moment < 1e-1:
+                    print("Stopping early...")
+                    break
 
         return state_spaces
+
+    def get_results(self):
+        res_dict = {}
+        res_dict["epoch_loss_history"] = np.array(self.loss_hist)
+        res_dict["init_kernel_cov"] = self._nonsingular_cov_from_triu(self.init_kernel_cov).detach().cpu().numpy().reshape((2, 2))
+        if self.dynamic_kernel:
+            res_dict["kernel_log_tau"] = self.kernel_log_tau.detach().cpu().numpy()
+            res_dict["final_kernel_cov"] = self._nonsingular_cov_from_triu(self.final_kernel_cov).detach().cpu().numpy().reshape((2, 2))
+        else:
+            res_dict["kernel_log_tau"] = None
+            res_dict["final_kernel_cov"] = None
+        res_dict["log_learn_rate"] = self.log_lr.detach().cpu().numpy()
+        res_dict["inverse_temperature"] = self.inverse_temperature.detach().cpu().numpy()
+        res_dict["initial_value_space"] = self.init_prob.detach().cpu().numpy()
+        res_dict["state_spaces"] = [space.detach().cpu().numpy() for space in self.state_spaces]
+        return res_dict
+    
+    
 
 
 
--- mturk2_code/sim/main.py
+++ mturk2_code/sim/main.py
@@ -1,116 +1,56 @@
-# # AUTHOR SPENCER LOGGIA
-# # INIT DATE 06/30/2021
-# from typing import Tuple, List
-#
-# import numpy as np
-# from scipy import stats
-#
-#
-# class GenModel:
-#
-#     def __init__(self, rewards: int = 4, modalities: int = 2, space_dims=2):
-#         self.mu = np.zeros((modalities, rewards, space_dims, 1))
-#         self.sigma = np.zeros((modalities, rewards, space_dims, space_dims))
-#         self.num_examples = np.zeros((modalities, rewards))
-#         self.modalities = modalities
-#         self.reward_freq = np.zeros(rewards)
-#         self.rewards = rewards
-#         self.trial_history = []
-#
-#     def fit_trial(self, x: np.ndarray, y: int):
-#         """
-#
-#         :param x: dim 0: space, dim 1: axis
-#         :param y: reward type / gmm component
-#         :return:
-#         """
-#         self.reward_freq[y] += 1
-#         self.trial_history.append((x, y))
-#         self.num_examples[:, y] += 1
-#         num_trials = self.num_examples[:, y]
-#         self.mu[:, y, :] = self.mu[:, y, :] + (x - self.mu[:, y, :]) / num_trials
-#         old_var = self.sigma[:, y, :, :]
-#         self.sigma[:, y, :, :] = old_var + (((x - self.mu) @ (x - self.mu).T) - old_var) / num_trials
-#
-#
-#     def predict_trial(self, x: np.ndarray, modality: int):
-#         """
-#         :param x: batch x shape dim
-#         :return: posterior probabilities of each reward value based on this modality
-#         """
-#         p_x_r = np.zeros(self.rewards)
-#
-#         for r in range(self.rewards):
-#             p_x_r[r] = stats.multivariate_normal.cdf(x, self.mu[modality, r, :], cov=self.sigma[modality, r, :, :])
-#         p_x = np.sum(p_x_r)
-#         p_r = self.reward_freq / np.sum(self.reward_freq)
-#         p_r_x = (p_x_r * p_r) / p_x
-#         return p_r_x
-#
-#
-#     def integrator(self, reward_prob: np.ndarray) -> np.ndarray:
-#         """
-#         optimally integrate the reward estimates from individual cues to estimate reward.
-#         :param reward_probs: From each modality the probabilities for each reward (m x r)
-#         :return integrated probabilities of each reward:
-#         """
-#         combined = np.prod(reward_prob, axis=0)
-#         return np.exp(combined) / np.exp(np.sum(combined))
-#
-#
-# class StimuliSpace:
-#     """
-#     A space contains n dim vectors of object coordinates, and gaussian mean coordinates, gaussian variance, and
-#     associated reward value
-#     """
-#     def __init__(self, coordinates, means, var, value):
-#         self.coordinates = coordinates
-#         self.means = means
-#         self.var = var
-#         self.values = value
-#
-#     def assign_reward(self, coord):
-#         probs = np.zeros(len(self.means))
-#         for i in range(len(self.means)):
-#             p_x_r = stats.multivariate_normal.cdf(coord, mu=self.means[i], cov=self.var[i])
-#             probs[i] = p_x_r
-#         exp_probs = np.exp(probs)
-#         probs = exp_probs / sum(exp_probs)
-#         reward = np.random.choice(np.arange(4), p=probs)
-#         return reward
-#
-#     def sample(self):
-#         idx = np.random.randint(len(self.coordinates))
-#         yield idx, self.assign_reward(self.coordinates[idx])
-#
-#
-# class Agent:
-#
-#     def __init__(self, num_iter, spaces: List[StimuliSpace], concurrent_choices, num_rewards, space_dims):
-#         self.spaces = spaces
-#         self.num_iter = num_iter
-#         self.concurrent = concurrent_choices
-#         self.model = GenModel(rewards=num_rewards, modalities=len(spaces), space_dims=space_dims)
-#         self.available_rewards = np.zeros(num_rewards)
-#         for i in range(self.concurrent):
-#
-#             for space in spaces:
-#                 item = space.sample()
-#
-#     def simulate(self):
-#         for i in range(self.concurent):
-#
-#
-#
-# def simulate(trials=100):
-#     color_space = SpaceSampler() # needs args
-#     shape_space = SpaceSampler() # needs args
-#     cs_gen = color_space.generate_samples()
-#     ss_gne = shape_space.generate_samples()
-#     for i in range(trials):
-#
-#
-#
-#
-#
-#
+import math
+
+import numpy as np
+import kernel_rl
+from matplotlib import pyplot as plt, animation
+import pandas as pd
+import pickle
+import sys
+
+
+if __name__=="__main__":
+    EPOCHS = int(sys.argv[3])
+    BATCH = int(sys.argv[2])
+    EXT = "TEST_2D_dynamic_kernel"
+    SUBJECT = sys.argv[1]
+    DEV = sys.argv[4]
+
+    title = "_".join([SUBJECT, EXT, str(EPOCHS), str(BATCH)])
+    print("starting", title)
+
+    plt.switch_backend('Qt5Agg')
+    subject_trials = "../saved_data/all_" + SUBJECT + ".csv"
+    data = pd.read_csv(subject_trials, sep="\t")
+    model = kernel_rl.KernelRL(resolution=36, device=DEV, expected_plateu=100000, kernel_mode="logistic")
+    model.fit(data.head(BATCH), epochs=EPOCHS, lr=.02)
+    res = model.get_results()
+
+    print("Final Params:")
+    for k in res.keys():
+        try:
+            if len(res[k].flatten()) < 10:
+                print(k, ":", res[k])
+        except Exception as e:
+            print(e)
+
+    fig, ax = plt.subplots(1)
+    ax.plot(res["epoch_loss_history"])
+
+    fig0, ax0 = plt.subplots(1)
+    ax0.imshow(res["initial_value_space"])
+
+    fig1, ax1 = plt.subplots()
+    states = np.array(res["state_spaces"])
+    vmin = np.min(states)
+    vmax = np.max(states)
+    # ax.imshow(param_states[0], vmin=vmin, vmax=vmax)
+    print("state min max", vmin, vmax)
+    param_im = [[ax1.imshow(states[si], vmin=vmin, vmax=vmax)] for si in range(0, len(states), math.ceil(np.log2(max(2, BATCH // 100))))]
+    ani = animation.ArtistAnimation(fig1, param_im, interval=10, blit=True, repeat_delay=100)
+
+    model.to("cpu")
+    with open("../saved_data/out_models/" + title + ".pkl", "wb") as f:
+        pickle.dump(model, f)
+
+    plt.show()
+
