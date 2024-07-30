ea3a1fd35089889b7e3e4d99df8b6ccca876ab94
Spencer R. Loggia <spencer.loggia98@gmail.com>
2024-07-29 22:23:02 -0400

    add forgetting

--- mturk2_code/sim/kernel_rl.py
+++ mturk2_code/sim/kernel_rl.py
@@ -4,9 +4,10 @@ import numpy as np
 from neurotools import util
 
 class KernelRL:
-    def __init__(self, device='cpu', precision="single", resolution=72, temporal_window=20, expected_plateu=100000, kernel_mode="logistic"):
+    def __init__(self, device='cpu', precision="single", resolution=72, temporal_window=20, expected_plateu=100000, kernel_mode="exp", forget_mode="stable"):
         """
         Fits monkey behavior by updating with a parameterized gaussian kernel.
+        All time measured in hours
         """
         self.s_real = 36
         self.c_real = 36
@@ -24,14 +25,24 @@ class KernelRL:
 
         self.temporal_window = temporal_window
 
-        self.init_prob = torch.nn.Parameter(torch.zeros((resolution,
-                                                         resolution), device=device, dtype=self.ndtype))
-        self.init_kernel_cov = torch.nn.Parameter(torch.tensor([.5, .01, .5], device=device, dtype=self.ndtype))
-        if self.dynamic_kernel:
-            # compute initial tau to reach 90% final at 1/2 number of trials 
-            tau = -2 * torch.log(torch.tensor([.1 / 1.9], dtype=self.ndtype, device=device)) / expected_plateu
-            self.kernel_log_tau = torch.nn.Parameter(torch.log(tau))
-            self.final_kernel_cov = torch.nn.Parameter(self.init_kernel_cov.clone())
+        self.init_prob = torch.nn.Parameter(torch.zeros((resolution, resolution), device=device, dtype=self.ndtype))
+        self.init_kernel_cov = torch.nn.Parameter(torch.tensor([.5, 0.0, .5], device=device, dtype=self.ndtype))
+        # compute initial tau to reach 63% final at 1/3 total trials
+        tau = -torch.log(torch.tensor([.63 * expected_plateu], dtype=self.ndtype, device=device))
+        self.kernel_log_tau = torch.nn.Parameter(torch.log(tau))
+        self.final_kernel_cov = torch.nn.Parameter(self.init_kernel_cov.clone())
+
+        self.forget_mode = forget_mode
+
+        # time constant for returning from current values state to initial (hours)
+        # start with being 63% forgotten after 4 days.
+        self.log_init_forget_tau = torch.nn.Parameter(-torch.log(torch.tensor([24.*4.*.63],
+                                                                              device=device, dtype=self.ndtype)))
+        self.log_final_forget_tau = torch.nn.Parameter(-torch.log(torch.tensor([24.*60.*.63],
+                                                                               device=device, dtype=self.ndtype)))
+        # the acceleration of forgetting, in trials.
+        self.log_forget_tau_tau = torch.nn.Parameter(-torch.log(torch.tensor([.63 * expected_plateu],
+                                                                               device=device, dtype=self.ndtype)))
 
         self.log_lr = torch.nn.Parameter(torch.tensor([-2], device=device, dtype=self.ndtype))
 
@@ -45,9 +56,11 @@ class KernelRL:
     def to(self, device):
         self.init_prob = torch.nn.Parameter(self.init_prob.to(device))
         self.init_kernel_cov = torch.nn.Parameter(self.init_kernel_cov.to(device))
-        if self.dynamic_kernel:
-            self.kernel_log_tau = torch.nn.Parameter(self.kernel_log_tau.to(device))
-            self.final_kernel_cov = torch.nn.Parameter(self.final_kernel_cov.to(device))
+        self.kernel_log_tau = torch.nn.Parameter(self.kernel_log_tau.to(device))
+        self.final_kernel_cov = torch.nn.Parameter(self.final_kernel_cov.to(device))
+        self.log_init_forget_tau = torch.nn.Parameter(self.log_init_forget_tau.to(device))
+        self.log_final_forget_tau = torch.nn.Parameter(self.log_final_forget_tau.to(device))
+        self.log_forget_tau_tau = torch.nn.Parameter(self.log_forget_tau_tau.to(device))
         self.log_lr = torch.nn.Parameter(self.log_lr.to(device))
         self.inverse_temperature = torch.nn.Parameter(self.inverse_temperature.to(device))
     
@@ -56,11 +69,14 @@ class KernelRL:
         <n, 4, 2> choices on range (0, s_real | c_real)
         """
         raw_choices = trial_data[["c1", "s1", "c2", "s2", "c3", "s3", "c4", "s4"]].to_numpy().reshape((-1, 4, 2))
+        time_stamps = trial_data[["time_delta_seconds"]].to_numpy()
+        time_stamps = time_stamps - np.concatenate([time_stamps[0], time_stamps[:-1]]) # every time stamp minus the one before
+        time_stamps = time_stamps / (60 * 60) # convert from seconds to hours.
         raw_choices = torch.from_numpy(raw_choices).to(self.ndtype).to(self.device)
         choices = raw_choices / torch.tensor([self.c_real, self.s_real], dtype=self.ndtype, device=self.device)[None, None, :]
         choice_idx = torch.from_numpy(trial_data.choice_idx.to_numpy()).long().to(self.device)
         reward = torch.from_numpy(trial_data.reward.to_numpy()).to(self.ndtype).to(self.device)
-        return torch.round(self.resolution * choices).int(), choice_idx, reward
+        return torch.round(self.resolution * choices).int(), choice_idx, reward, time_stamps
 
     def _nonsingular_cov_from_triu(self, vec):
         cov_in = util.triu_to_square(vec, 2, includes_diag=True)
@@ -72,31 +88,41 @@ class KernelRL:
         """
         ce = torch.nn.CrossEntropyLoss()
         state_spaces = None
-        options, choice_idxs, rewards = self._get_choices_rewards(trial_data)
-        if not self.dynamic_kernel:
-            self.optimizer = torch.optim.Adam(params=[self.init_prob, self.init_kernel_cov,
-                                                      self.inverse_temperature, self.log_lr],
-                                              lr=lr)
-        else:
-            self.optimizer = torch.optim.Adam(params=[self.init_kernel_cov, self.kernel_log_tau,
-                                                      self.final_kernel_cov, self.inverse_temperature, self.log_lr],
-                                              lr=lr)
+        options, choice_idxs, rewards, time_delta = self._get_choices_rewards(trial_data)
+
+        self.optimizer = torch.optim.Adam(params=[self.init_kernel_cov, self.kernel_log_tau,
+                                                  self.final_kernel_cov, self.inverse_temperature, self.log_lr,
+                                                  self.log_init_forget_tau, self.log_final_forget_tau, self.log_forget_tau_tau],
+                                          lr=lr)
         for epoch in range(epochs):
             loss = torch.zeros((1,), dtype=self.ndtype, device=self.device)
             self.state_spaces = []
             print("Epoch", epoch)
             self.optimizer.zero_grad()
             state_space = self.init_prob.clone()
-            # iterate through all trials
+            # build true nonsingular cov from cov input
+            kernel_cov = self._nonsingular_cov_from_triu(self.init_kernel_cov)
+            f_kernel_cov = self._nonsingular_cov_from_triu(self.final_kernel_cov)
+            # select the input covariance matrix at this time point
+            kernel_covs = util.exponential_func(torch.arange(len(trial_data)), kernel_cov.flatten(),
+                                               f_kernel_cov.flatten(), self.kernel_log_tau).reshape((-1, 2, 2))
+            # get the forget time constants for all trials
+            log_forget_taus = torch.log(util.exponential_func(torch.arange(len(trial_data)),
+                                                              torch.exp(self.log_init_forget_tau),
+                                                              torch.exp(self.log_final_forget_tau),
+                                                              self.log_forget_tau_tau).flatten())
 
+            # iterate through all trials
             for ind in range(len(trial_data)):
-                # build true nonsingular cov from cov input
-                kernel_cov = self._nonsingular_cov_from_triu(self.init_kernel_cov)
-                if self.dynamic_kernel:
-                    f_kernel_cov = self._nonsingular_cov_from_triu(self.final_kernel_cov)
-                    # select the input covariance matrix at this time point
-                    kernel_cov = util.logistic_func(torch.tensor([ind]), kernel_cov.flatten(),
-                                                 f_kernel_cov.flatten(), self.kernel_log_tau).reshape((2, 2))
+                kernel_cov = kernel_covs[ind]
+                time = time_delta[ind]
+                log_forget_tau = log_forget_taus[ind]
+                # compute how much we forgot since the last trial
+                cur_state = util.exponential_func(torch.tensor([time]), state_space.flatten(),
+                                                  self.init_prob.clone().flatten(),
+                                                  log_forget_tau).reshape(state_space.shape)
+                state_space = cur_state
+
                 # construct gaussian kernel (uses spencer's tools library)
                 kernel = util.gaussian_kernel(kernel_size=(self.resolution, self.resolution),
                                               cov=kernel_cov * self.resolution,
@@ -143,12 +169,11 @@ class KernelRL:
         res_dict = {}
         res_dict["epoch_loss_history"] = np.array(self.loss_hist)
         res_dict["init_kernel_cov"] = self._nonsingular_cov_from_triu(self.init_kernel_cov).detach().cpu().numpy().reshape((2, 2))
-        if self.dynamic_kernel:
-            res_dict["kernel_log_tau"] = self.kernel_log_tau.detach().cpu().numpy()
-            res_dict["final_kernel_cov"] = self._nonsingular_cov_from_triu(self.final_kernel_cov).detach().cpu().numpy().reshape((2, 2))
-        else:
-            res_dict["kernel_log_tau"] = None
-            res_dict["final_kernel_cov"] = None
+        res_dict["kernel_log_tau"] = self.kernel_log_tau.detach().cpu().numpy()
+        res_dict["final_kernel_cov"] = self._nonsingular_cov_from_triu(self.final_kernel_cov).detach().cpu().numpy().reshape((2, 2))
+        res_dict["init_forget_log_tau"] = self.log_init_forget_tau.detach().cpu().numpy()
+        res_dict["final_forget_log_tau"] = self.log_final_forget_tau.detach().cpu().numpy()
+        res_dict["forget_log_tau_tau"] = self.log_forget_tau_tau.detach().cpu().numpy()
         res_dict["log_learn_rate"] = self.log_lr.detach().cpu().numpy()
         res_dict["inverse_temperature"] = self.inverse_temperature.detach().cpu().numpy()
         res_dict["initial_value_space"] = self.init_prob.detach().cpu().numpy()
--- mturk2_code/sim/main.py
+++ mturk2_code/sim/main.py
@@ -1,28 +1,61 @@
 import math
+import argparse
+import os.path
+
 
 import numpy as np
+import torch
+
 import kernel_rl
 from matplotlib import pyplot as plt, animation
 import pandas as pd
 import pickle
 import sys
+from neurotools import util, support
 
 
 if __name__=="__main__":
-    EPOCHS = int(sys.argv[3])
-    BATCH = int(sys.argv[2])
-    EXT = "TEST_2D_dynamic_kernel"
-    SUBJECT = sys.argv[1]
-    DEV = sys.argv[4]
+    parser = argparse.ArgumentParser(prog="KernelRL")
+    parser.add_argument("name", type=str, choices=["Sally", "Tina", "Yuri", "Buzz"])
+    parser.add_argument("-l", "--load", nargs=1, type=str, action="store")
+    parser.add_argument("-t", "--train", nargs=2, type=int, action="append")
+    parser.add_argument("--cuda", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.train:
+        EPOCHS = args.train[0][1]
+        BATCH = args.train[0][0]
+    else:
+        EPOCHS = None
+        BATCH = None
+    EXT = "2D_dynamic_kernel"
+    RES = 36
+
+    SUBJECT = args.name
+    if args.cuda:
+        DEV = "cuda"
+    else:
+        DEV = "cpu"
 
     title = "_".join([SUBJECT, EXT, str(EPOCHS), str(BATCH)])
     print("starting", title)
+    figure_out_dir = "../saved_data/out_rl_figs/"
 
     plt.switch_backend('Qt5Agg')
     subject_trials = "../saved_data/all_" + SUBJECT + ".csv"
     data = pd.read_csv(subject_trials, sep="\t")
-    model = kernel_rl.KernelRL(resolution=36, device=DEV, expected_plateu=100000, kernel_mode="logistic")
-    model.fit(data.head(BATCH), epochs=EPOCHS, lr=.02)
+
+    if args.load:
+        out_path = args.load[0]
+        with open(args.load[0], "rb") as f:
+            model = pickle.load(f)
+    else:
+        out_path = "../saved_data/out_models/" + title + ".pkl"
+        model = kernel_rl.KernelRL(resolution=RES, device=DEV, expected_plateu=100000, kernel_mode="logistic")
+
+    if args.train:
+        model.fit(data.head(BATCH), epochs=EPOCHS, lr=.02)
     res = model.get_results()
 
     print("Final Params:")
@@ -45,11 +78,28 @@ if __name__=="__main__":
     vmax = np.max(states)
     # ax.imshow(param_states[0], vmin=vmin, vmax=vmax)
     print("state min max", vmin, vmax)
-    param_im = [[ax1.imshow(states[si], vmin=vmin, vmax=vmax)] for si in range(0, len(states), math.ceil(np.log2(max(2, BATCH // 100))))]
+    step_size = math.ceil(np.log2(max(2, len(states) // 100)))
+    state_im = [states[si] for si in range(0, len(states), step_size)]
+    param_im = [[ax1.imshow(s, vmin=vmin, vmax=vmax)] for s in state_im]
     ani = animation.ArtistAnimation(fig1, param_im, interval=10, blit=True, repeat_delay=100)
+    state_out = os.path.join(figure_out_dir, "state_anim_" + title + ".avi")
+    support.save_video_from_arrays(state_im, vmin, vmax, state_out)
+
+    fig2, ax2 = plt.subplots()
+    in_cov = torch.from_numpy(res["init_kernel_cov"])
+    f_cov = torch.from_numpy(res["final_kernel_cov"])
+    tau = torch.from_numpy(res["kernel_log_tau"])
+    cov_t = util.exponential_func(torch.arange(0, len(states), step_size), in_cov.flatten(), f_cov.flatten(), tau)
+    kernels = [util.gaussian_kernel((RES, RES), cov=c.reshape((2, 2)) * RES, renormalize=True).detach().numpy() for c in cov_t]
+    vmin = np.min(np.stack(kernels))
+    vmax = np.max(np.stack(kernels))
+    k_im = [[ax2.imshow(k, vmin=vmin, vmax=vmax)] for k in kernels]
+    ani = animation.ArtistAnimation(fig2, k_im, interval=10, blit=True, repeat_delay=100)
+    state_out = os.path.join(figure_out_dir, "kernel_anim_" + title + ".avi")
+    support.save_video_from_arrays(kernels, vmin, vmax, state_out)
 
     model.to("cpu")
-    with open("../saved_data/out_models/" + title + ".pkl", "wb") as f:
+    with open(out_path, "wb") as f:
         pickle.dump(model, f)
 
     plt.show()
